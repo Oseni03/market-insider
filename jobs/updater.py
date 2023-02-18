b79a54ecc9d384a01c8ba56239c42f0d702a4a93
Oseni03 <oseniayomide57@gmail.com>
2023-02-18 15:20:49 +0100

    Newsletter cronjob removed

--- jobs/updater.py
+++ jobs/updater.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 
-from jobs.jobs import API_SCHEDULAR, Newsletter
+from jobs.jobs import API_SCHEDULAR#, Newsletter
 
 
 def start():
@@ -9,7 +9,7 @@ def start():
     scheduler = BackgroundScheduler()
     task = API_SCHEDULAR()
     scheduler.add_job(task.run, "interval", minutes=180)
-    newsletter = Newsletter()
-    scheduler.add_job(newsletter.main, "interval", minutes=360)
+    # newsletter = Newsletter()
+    # scheduler.add_job(newsletter.main, "interval", minutes=360)
     scheduler.start()
     print("Ending CronJob")
